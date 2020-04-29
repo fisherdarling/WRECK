83886420c604141da5e5528177bedcfa871dac71
Fisher Darling <fdarlingco@gmail.com>
2020-04-29 15:55:16 -0600

    Added asterix (doesn't work) created an AST module

---
+++ src/ast.rs
@@ -0,0 +1,5 @@
+use silly_lex::Token;
+
+pub struct CharRng {
+    dash: Token,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
 use silly_lex::{Lexer, Token};
 
+pub mod ast;
+// ast!(
+//     Lit |isize|,
+//     Expr: enum Expr {
+//         BinOp: struct BinOp {
+//             op: enum Op {
+//                 Plus,
+//                 Minus,
+//                 Times,
+//                 Divide,
+//             },
+//             lhs: Box<Expr>,
+//             rhs: Box<Expr>,
+//         },
+//         |Lit|
+//     }
+// );
+
 #[cfg(test)]
 mod tests {
     #[test]
