c01c42518969b31190a0bbce4aac7ca6a2e6cffa
Fisher Darling <fdarlingco@gmail.com>
2020-04-29 16:24:32 -0600

    Added an AstNode and AstKind

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,30 @@
 use silly_lex::Token;
 
-pub struct CharRng {
-    dash: Token,
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum AstKind {
+    Regex,
+    Alt,
+    AltList,
+    Seq,
+    SeqList,
+    Atom,
+    AtomMod(Mod),
+    Nucleus,
+    CharRng,
+    Lambda,
+    Dot,
+    Char(char),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct AstNode {
+    pub kind: AstKind,
+    pub children: Option<Vec<AstNode>>,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Mod {
+    Kleene,
+    Plus,
+    Lambda,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,6 @@
 use silly_lex::{Lexer, Token};
 
 pub mod ast;
-// ast!(
-//     Lit |isize|,
-//     Expr: enum Expr {
-//         BinOp: struct BinOp {
-//             op: enum Op {
-//                 Plus,
-//                 Minus,
-//                 Times,
-//                 Divide,
-//             },
-//             lhs: Box<Expr>,
-//             rhs: Box<Expr>,
-//         },
-//         |Lit|
-//     }
-// );
 
 #[cfg(test)]
 mod tests {
