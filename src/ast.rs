404dc7efd565e7313e9fcbdeea0c36ce2a5a8852
Liam Warfield <liam.warfield@gmail.com>
2020-04-29 20:58:42 -0600

    Working my way up to blue circuts.

--- src/ast.rs
+++ src/ast.rs
@@ -9,12 +9,23 @@ pub enum AstKind {
     Seq,
     SeqList,
     Atom,
-    AtomMod(Mod),
     Nucleus,
     CharRng,
+    Kleene,
+    Plus,
     Lambda,
     Dot,
     Char(char),
+}
+
+impl AstKind {
+    pub fn char(&self) -> anyhow::Result<char> {
+        if let AstKind::Char(c) = self {
+            Ok(*c)
+        } else {
+            Err(anyhow::anyhow!("AstKind was not a Char"))
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -23,41 +34,67 @@ pub struct AstNode {
     pub children: Vec<AstNode>,
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Mod {
-    Kleene,
-    Plus,
-    Lambda,
+impl AstNode {
+    pub fn new(kindr: AstKind) -> Self {
+        AstNode {
+            kind: kindr,
+            children: vec![],
+        }
+    }
 }
 
 // simplify a node and all children
-pub fn simplify_to_ast(node: AstNode) -> AstNode {
+pub fn simplify_node(node: AstNode) -> AstNode {
     match node.kind {
-        AstKind::Regex => simplify_regex(node),
-        AstKind::Atom => simplify_atom(node),
-        AstKind::Seq => simplify_seq(node),
-        AstKind::SeqList => simplify_seq_list(node),
-        AstKind::CharRng => simplify_char_rng(node),
+        AstKind::Atom => simplify_atom(&node),
+        AstKind::Seq => simplify_seq(&node),
+        AstKind::SeqList => simplify_seq_list(&node),
+        AstKind::CharRng => simplify_char_rng(&node),
         _ => node,
     }
 }
 
-pub fn simplify_regex(node: AstNode) -> AstNode {
-    simplify_to_ast(node.children[0].clone())
-}
 
-pub fn simplify_atom(node: AstNode) -> AstNode {
+pub fn simplify_seq(node: &AstNode) -> AstNode {
     todo!()
 }
 
-pub fn simplify_seq(node: AstNode) -> AstNode {
+pub fn simplify_seq_list(node: &AstNode) -> AstNode {
     todo!()
 }
 
-pub fn simplify_seq_list(node: AstNode) -> AstNode {
+pub fn simplify_char_rng(node: &AstNode) -> AstNode {
     todo!()
 }
 
-pub fn simplify_char_rng(node: AstNode) -> AstNode {
-    todo!()
+pub fn simplify_atom(atom_node: &AstNode) -> AstNode {
+    let mut nucleus = simplify_nucleus(&atom_node.children[0]);
+    let atom_mod = &atom_node.children[1].children[0];
+    let mut new_atom;
+    match atom_mod.kind {
+        AstKind::Kleene => new_atom = AstNode::new(AstKind::Kleene),
+        AstKind::Plus => new_atom = AstNode::new(AstKind::Plus),
+        AstKind::Lambda => new_atom = AstNode::new(AstKind::Alt),
+        _ => panic!("Bad Atom Mod")
+    }
+    new_atom.children.append(&mut nucleus.children);
+    new_atom
+}
+
+pub fn simplify_nucleus(nucleus_node: &AstNode) -> AstNode {
+    let mut new_nuc = AstNode::new(AstKind::Nucleus);
+    new_nuc.children.push(nucleus_node.children[0].clone());
+
+    if nucleus_node.children[1].children.len() > 1 {
+        let c = nucleus_node.children[0].kind.char().unwrap();
+        let m = nucleus_node.children[1].children[1].kind.char().unwrap();
+        let start: u8 = c as u8;
+        let end: u8 = m as u8;
+        let range = start..=end;
+        
+        for i in range {
+            new_nuc.children.push(AstNode::new(AstKind::Char(i as char)));
+        }
+    } 
+    new_nuc
 }
